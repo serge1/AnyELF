5410f1e220e75a7f22ccb072e6c687f4d7a13c8f
Serge Lamikhov-Center <to_serge@users.sourceforge.net>
2013-05-26 18:55:04 +0300

    Dump for section and segment data added; Version increase

--- make_package.bat
+++ make_package.bat
@@ -1,3 +1,3 @@
 msbuild anyelf.vcxproj /t:Rebuild /p:project="anyelf" /p:platform="Win32" /p:configuration="Release"
 msbuild anyelf.vcxproj /t:Rebuild /p:project="anyelf" /p:platform="x64" /p:configuration="Release"
-cscript zip.vbs wlx_anyelf_1.2.zip Release\anyelf.wlx x64\Release\anyelf.wlx64 pluginst.inf
+cscript zip.vbs wlx_anyelf_1.3.zip Release\anyelf.wlx x64\Release\anyelf.wlx64 pluginst.inf
